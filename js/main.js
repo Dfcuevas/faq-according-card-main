74723550d8a74f821615c317c722b7095061bd64
Diego Cuevas <ingdcuevas@gmail.com>
2021-10-08 21:30:33 -0500

    trabajando en la logica de la pagina

--- js/main.js
+++ js/main.js
@@ -1,31 +1,12 @@
 let question = document.getElementsByClassName('question')
-let containers = document.getElementsByClassName('faq-container')
-let arrows = document.getElementsByClassName('arrow-img')
 
-for(index of question) {
-    index.addEventListener('click', mostrarAnswer)
-}
-
-function mostrarAnswer (e) {
-
-    if(e.target.nextElementSibling.classList.contains('hidden-answer')) {
-        e.target.nextElementSibling.classList.remove('hidden-answer')
+document.addEventListener('click', function (e) {
+    if(e.target.classList.contains('question')) {
         e.target.style.fontWeight = '700'
-        
-    } else if (!e.target.nextElementSibling.classList.contains('hidden-answer')) {
-        e.target.nextElementSibling.classList.add('hidden-answer')
-        e.target.style.fontWeight = '400'
+        e.target.nextElementSibling.classList.add('show-answer')
+        e.target.parentElement.nextElementSibling.classList.add('rotate-arrow')
+        // console.log(e.target.parentElement.nextElementSibling)
     }
     
-}
-
-const rotarArrows = (e) => {
-    e.target.style.transform = 'rotate(180deg)'
-}
-
-
-for (imgArrow of arrows) {
-    imgArrow.addEventListener('click', rotarArrows)
-}
-
+})
 
